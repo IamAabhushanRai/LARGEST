933c4795d94f5e4816cc4f6d28a38f9044eab288
IamAabhushanRai <rai.aabhushan@gmail.com>
2024-08-23 17:53:21 +0545

    remotely added

---
+++ day1.py
@@ -0,0 +1,35 @@
+# this is a comment jhfjhfhgfhfhjgf hfhgf
+# print(2)
+# hjfhjgjh hghjh
+
+
+'''this is commn===
+hell_'''
+number=1
+
+print(number)
+# 1number=12
+# @number=12
+# print (@number)
+_try=12
+print (_try)
+# my phone=1234
+
+# my-phone=1234
+'''thsi is wrong
+and it is not enough 
+so we cant go beyond it 
+thank you.'''
+
+year=2024
+age=26
+born=year-age
+print(born)
+# finding date of birth "logic"
+
+age=33
+print(type(age))
+
+add="1"*"2"
+print(add)
+
